791d8f9a9d2918e3fb372bf3ca9f8d8f81276253
zorro901 <zoro907@gmail.com>
2021-08-30 23:16:32 +0900

    20. CRUD(Hasura) memo

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -13,8 +13,10 @@ import {
   UpdateUserMutation
 } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+import { UserItem } from '../components/UserItem'
 
 const HasuraCRUD: VFC = () => {
+  const [editedUser, setEditedUser] = useState({id: '', name: ''})
   const {data, error} = useQuery<GetUsersQuery>(GET_USERS, {
     fetchPolicy: 'cache-and-network'
   })
@@ -44,12 +46,68 @@ const HasuraCRUD: VFC = () => {
       })
     }
   })
-
-
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedUser.id) {
+      try {
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name
+          }
+        })
+      } catch (e) {
+        alert(e.message)
+      }
+      setEditedUser({id: '', name: ''})
+    } else {
+      try {
+        await insert_users_one({
+          variables: {
+            name: editedUser.name
+          }
+        })
+      } catch (e) {
+        alert(e.message)
+      }
+      setEditedUser({id: '', name: ''})
+    }
+  }
+  if (error) return <Layout title={'Hasura CRUD'}>Error: {error.message}</Layout>
   return (
     <Layout title={'Hasura CRUD'}>
       <p className={'mb-3 font-bold'}>Hasura CRUD</p>
+      <form
+        className={'flex flex-col justify-center items-center'}
+        onSubmit={handleSubmit}>
+        <input
+          className={'px-3 py-2 border border-gray-300'}
+          placeholder={'New user ?'}
+          type={'text'}
+          value={editedUser.name}
+          onChange={(e) =>
+            setEditedUser({...editedUser, name: e.target.value})}
+        />
+        <button
+          disabled={!editedUser.name}
+          className={'disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none'}
+          data-testid="new"
+          type="submit"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+        </button>
+      </form>
+      {data?.users.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            user={user}
+            setEditedUser={setEditedUser}
+            delete_users_by_pk={delete_users_by_pk}
+          />
+        )
+      })}
     </Layout>
-
   )
 }
+export default HasuraCRUD
