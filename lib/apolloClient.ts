b22fcce6712b7210b1531b389e93b8c9e0d4546d
zorro901 <zoro907@gmail.com>
2021-09-01 01:58:02 +0900

    28. Deploy to Vercel

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -6,7 +6,7 @@ import gql, {
 } from '@apollo/client'
 import 'cross-fetch/polyfill'
 
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+// export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
 
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 
@@ -15,7 +15,10 @@ const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: 'https://basic-lesson-hasura-200.hasura.app/v1/graphql' // Server URL (must be absolute)
+      uri: process.env.NEXT_PUBLIC_HASURA_URL, // Server URL (must be absolute)
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY
+      }
     }),
     cache: new InMemoryCache()
   })
